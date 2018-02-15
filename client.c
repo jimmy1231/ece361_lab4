2bad03cec0adf9c53bd4299baa8f1c942d90f639
Li  Jing Yi <lijing53@ug230.eecg.toronto.edu>
2018-02-15 15:00:17 -0500

    Committing updated files for client and server

--- client.c
+++ client.c
@@ -11,14 +11,66 @@
  * Created on February 15, 2018, 1:39 PM
  */
 
+
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#define DEBUG(fmt, args...) (printf(fmt, ##args))
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+//    int server_addr = atoi(argv[1]);
+//    int server_port = atoi(argv[2]);
+//    char *user_name = argv[3];
+//    DEBUG("Config Parameters:\n   Server Address: %d\n   Server Port: %d\n   User Name: %s\n", server_addr, server_port, user_name);
+    
+    // create a socket 
+    int client_socket;
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    return (EXIT_SUCCESS);
+    // connect to server
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(client_socket, (struct sockaddr *) &server_address, sizeof (server_address));
+
+    // check for error with the connection
+    if (connection_status == -1) {
+        DEBUG("THERE was an error making a connection to the remote socket \n\n");
+    }
+    
+    DEBUG("Connection Established!\n");
+    
+    while (1) {
+        char msg[256];
+        printf("Enter message you want to send (Max. 128 chars):");
+        fgets(msg, 256, stdin);
+        DEBUG("You want to send: %s", msg);
+        
+        // Send something to server, now that we've got the connection set up
+        int send_status = send(client_socket, msg, sizeof(msg), 0);
+
+        while (send_status < 0) DEBUG("Send Error!!!!"); 
+
+        // receive data from the server:
+        char server_response[256];
+        recv(client_socket, &server_response, sizeof (server_response), 0);
+
+        DEBUG("The server sent the data: %s\n", server_response);
+    }
+    
+    close(client_socket);
+
+    return 0;
 }
+
 
--- server.c
+++ server.c
@@ -13,12 +13,68 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+#define DEBUG(fmt, args...) (printf(fmt, ##args))
 /*
  * 
  */
-int main(int argc, char** argv) {
 
-    return (EXIT_SUCCESS);
+void setup_sock_bind(int *, int);
+void poll_for_client_connection(int *, int);
+
+int main(int argc, char** argv) {
+//    DEBUG("arg[1]: %s\n", argv[1]);
+
+    // Server Bootstrap
+    int server_socket;
+    setup_sock_bind(&server_socket, 9002);
+    listen(server_socket, 5);
+
+    // Wait for client to connect
+    int client_socket;
+    poll_for_client_connection(&client_socket, server_socket);
+
+    while (1) {
+        // Wait 
+        char recv_message[256];
+        int recv_size = recv(client_socket, &recv_message, sizeof (recv_message), 0);
+        DEBUG("Received Message from Client, Size: %d, Content: %s\n", recv_size, recv_message);
+
+        // Send a message from server back to client
+        char server_message[256] = "You have reached the server!";
+//        strcat(server_message, recv_message);
+        send(client_socket, server_message, sizeof (server_message), 0);
+    }
+
+
+    close(server_socket);
+
+    return 0;
+}
+
+void setup_sock_bind(int *server_socket, int port_num) {
+    *server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port_num);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    bind(*server_socket, (struct sockaddr*) &server_address, sizeof (server_address));
+}
+
+void poll_for_client_connection(int* client_socket, int server_socket) {
+
+    // Wait state for server to receive client connection
+    *client_socket = accept(server_socket, NULL, NULL);
+
+    DEBUG("Connection has been established from: %d\n");
+
 }
 
