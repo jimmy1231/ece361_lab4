04418c9b58cae20e7aea9b2a638b9d96391ed0aa
jimmy1231 <dfjimmy.lli@gmail.com>
2018-02-16 23:25:54 -0600

    Client Server Added

--- client.c
+++ client.c
@@ -19,6 +19,7 @@
 #include <sys/socket.h>
 
 #include <netinet/in.h>
+#include <pthread.h>
 
 #define AUTHENTICATE 2
 #define BROADCAST 3
@@ -43,25 +44,36 @@ int is_broadcast(char *);
 void handle_broadcast();
 int send_to_server(int, char *);
 void handle_pmessage();
-void print_response(int, char *);
+void print_response(char *);
 
 void format_message(int, char *, char *);
 int authenticate(char *);
 
+void *listen_to_server();
+
 int client_socket;
+int auth = -1;
+int ready = 0;
 
 int main(int argc, char** argv) {
-//    int server_addr = atoi(argv[1]);
-//    int server_port = atoi(argv[2]);
-//    char *user_name = argv[3];
-    char *mock_username = "Jimmy";
+   // int server_addr = atoi(argv[1]);
+   // int server_port = atoi(argv[2]);
+   char *mock_username = argv[1];
+    // char *mock_username = "Jimmy";
 //    DEBUG("Config Parameters:\n   Server Address: %d\n   Server Port: %d\n   User Name: %s\n", server_addr, server_port, user_name);
 
+    pthread_t listen;
     // create a socket
     setup_sock();
 
     // connect to server
     struct sockaddr_in server_address;
+
+    int x = 0;
+    if (pthread_create(&listen, NULL, listen_to_server, &x)) {
+      DEBUG("Error Creating Thread\n");
+    }
+
     int connection_status = setup_connection(&server_address, client_socket, mock_username);
 
     // check for error with the connection
@@ -70,17 +82,54 @@ int main(int argc, char** argv) {
 
     } else {
         DEBUG("Connection Established! Welcome %s\n", mock_username);
+
         while (1) {
             char command[MSG_SIZE];
 
             get_command(command);
             if (!parse_command(command)) break;
+
+            // Wait for listen thread to print out message..
+            int i = 0;
+            while (++i < 1000000000);
         }
+    }
+
+    if (pthread_join(listen, NULL)) {
+      DEBUG("Error Joining Thread\n");
     }
 
     // Graceful close, Flag=2 means stop sending and receiving messages from this connection
     shutdown(client_socket, 2);
     return 0;
+}
+
+void *listen_to_server(void *ptr) {
+  DEBUG("HI WE'RE IN A THREAD!!");
+
+  while (1) {
+    char server_response[256];
+
+    recv(client_socket, &server_response, sizeof (server_response), 0);
+    DEBUG("\nPTHREAD: ");
+    print_response(server_response);
+
+    char auth_msg[256] = "AUTHENTICATE AUTH";
+    if (strstr(server_response, "AUTHENTICATE")) {
+      if (strcmp(server_response, auth_msg) != 0) {
+          printf("Authentication Failure: %s\n", (char *)server_response);
+          ready = 1;
+          auth = 0;
+      }
+      else {
+        DEBUG("Connection successful. Server Response: %s\n\n", server_response);
+        ready = 1;
+        auth = 1;
+      }
+    }
+  }
+
+  return NULL;
 }
 
 void setup_sock() {
@@ -104,18 +153,14 @@ int authenticate(char *username) {
     DEBUG("Authentication resend attempts: %d\n", send_attempts);
     if (send_attempts == CONNECTION_TIMEOUT) return -1;
 
-    recv(client_socket, &server_response, sizeof (server_response), 0);
+    // recv(client_socket, &server_response, sizeof (server_response), 0);
 
     // Parse Server response to see if authenticated
-    char auth[256] = "AUTH";
-    if (strcmp(server_response, auth) != 0) {
-        printf("Authentication Failure: %s\n", (char *)server_response);
-        return 0;
-    }
-    else {
-      DEBUG("Connection successful, Username: %s. Server Response: %s\n\n", username, server_response);
-      return 1;
-    }
+    // char auth[256] = "AUTH";
+    while (ready == 0 || auth < 0);
+    if (auth) return 1;
+    else return 0;
+
 }
 
 int setup_connection(struct sockaddr_in * server_address, int client_socket, char *username) {
@@ -217,8 +262,8 @@ int send_to_server(int type, char *body) {
 
         if (send_status > 0) {
             // receive data from the server:
-            recv(client_socket, &server_response, sizeof (server_response), 0);
-            print_response(type, server_response);
+            // recv(client_socket, &server_response, sizeof (server_response), 0);
+            // print_response(type, server_response);
         } else {
             DEBUG("[BROADCAST]: SOMETHING WENT WRONGGGGG :(");
         }
@@ -231,7 +276,7 @@ int send_to_server(int type, char *body) {
     }
 }
 
-void print_response(int type, char *server_response) {
+void print_response(char *server_response) {
     printf("Server Sent: %s\n\n---------------------------------------------------------\n\n", server_response);
 }
 
